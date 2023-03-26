501646cd8904480549cbeaa95ad36417607a7435
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-03-26 23:28:29 +0200

    render images

--- store/models.py
+++ store/models.py
@@ -15,10 +15,18 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-
+    image = models.ImageField(null= True, blank= True)
+    
     def __str__(self) -> str:
         return self.name
     
+    @property
+    def image_url(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, null=True, blank=True, on_delete=models.SET_NULL)
