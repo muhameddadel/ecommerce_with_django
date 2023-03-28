e47d6f61ebdeae65744b84bcbc7d72ceac5fb331
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-03-28 23:18:30 +0200

    get total items

--- store/views.py
+++ store/views.py
@@ -13,9 +13,10 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-
+        order= {"get_cart_total": 0, 'get_cart_items': 0}
     context = {
-        'items': items
+        'items': items,
+        'order': order
     }
     return render(request, 'store/cart.html', context)
 
